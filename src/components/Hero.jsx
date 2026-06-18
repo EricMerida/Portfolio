e780fbd4c9bfd00ca24df2c42c119981ada75327
Eric Merida <emerida623@gmail.com>
2026-06-18 15:02:12 -0400

    Refactor Hero component structure and content

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -25,7 +25,7 @@ export default function Hero() {
 
           <p className={styles.sub}>
             I transitioned into software engineering after building careers in real estate
-            and fine dining hospitality, where i developed strong communication, problem solving,
+            and fine dining hospitality, where I developed strong communication, problem solving,
             and client-focused skills. Today, I build full-stack applications using MERN stack,
             creating responsive user interface and scalable backend systems. My background
             allows me to combine technical expertise with a deep understanding of customer needs 
