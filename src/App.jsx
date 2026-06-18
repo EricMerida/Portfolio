e1bc3db7fdc5db2ad8563ed6099623b6a40329a4
Eric Merida <emerida623@gmail.com>
2026-06-18 12:15:56 -0400

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import { ThemeProvider } from './context/ThemeContext'
 import Navbar from './components/Navbar'
 import Hero from './components/Hero'
 import Projects from './components/Projects'
@@ -5,18 +6,20 @@ import Skills from './components/Skills'
 import Contact from './components/Contact'
 import Footer from './components/Footer'
 import './styles/global.css'
- 
+
 export default function App() {
   return (
-    <div className="portfolio">
+    <ThemeProvider>
       <Navbar />
-      <main>
-        <Hero />
-        <Projects />
-        <Skills />
-        <Contact />
-      </main>
-      <Footer />
-    </div>
+      <div className="portfolio">
+        <main>
+          <Hero />
+          <Projects />
+          <Skills />
+          <Contact />
+        </main>
+        <Footer />
+      </div>
+    </ThemeProvider>
   )
 }
