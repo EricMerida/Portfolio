2314572fe65c992a43655c32fd991712be36e378
Eric Merida <emerida623@gmail.com>
2026-06-21 12:28:07 -0400

    Reorder components in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
 import { ThemeProvider } from './context/ThemeContext'
 import Navbar from './components/Navbar'
+import Intro from './components/Intro'
 import Hero from './components/Hero'
-import Projects from './components/Projects'
+import About from './components/About'
 import Skills from './components/Skills'
+import Projects from './components/Projects'
 import Contact from './components/Contact'
 import Footer from './components/Footer'
 import './styles/global.css'
@@ -11,11 +13,13 @@ export default function App() {
   return (
     <ThemeProvider>
       <Navbar />
+      <Intro />
       <div className="portfolio">
         <main>
           <Hero />
-          <Projects />
+          <About />
           <Skills />
+          <Projects />
           <Contact />
         </main>
         <Footer />
